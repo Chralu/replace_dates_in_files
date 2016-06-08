f119ec042411aae5c86af2bb5e92b9c9f22a8abb
Charly Caulet <ccaulet@sqli.com>
2016-06-08 10:25:12 +0200

    Renaming package to 'replace_dates_in_files'

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
-setup(name='rename_files',
+setup(name='replace_dates_in_files',
       version='3.2',
       description='Met a jour les dates dans les noms de fichiers et le contenu des fichiers.',
       author='Charly Caulet',
-      author_email='ccaulet@sqli.com',
-      url='https://github.com/Chralu/gandyn/',
+      author_email='contact@charly-caulet.net',
+      url='https://github.com/Chralu/replace_dates_in_files',
       install_requires=['watchdog'],
       packages=[],
-      scripts=['rename_files.py'],
+      scripts=['replace_dates_in_files.py'],
       )
